99663d2d648f2e219cd00aba58e005c2dc828667
ervinwirth <86844816+poetyi@users.noreply.github.com>
2024-11-16 20:39:42 +0100

    Add language change to main screen; add language files

--- lib/main.dart
+++ lib/main.dart
@@ -2,31 +2,59 @@ import 'package:flutter/material.dart';
 import '/src/data/app_database.dart';
 import 'create_project_screen.dart';
 import 'project_list_screen.dart';
+import 'l10n/generated/l10n.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale _locale = const Locale('en'); // Default language is English
+
+  void setLocale(Locale locale) {
+    setState(() {
+      _locale = locale;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Agrisix App',
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: _locale,
+      onGenerateTitle: (BuildContext context) => S.of(context).appTitle,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Agrisix Home'),
+      home: Builder(
+        builder: (context) => MyHomePage(
+          onLanguageChanged: (Locale locale) => setLocale(locale),
+        ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+  final Function(Locale) onLanguageChanged;
 
-  final String title;
+  const MyHomePage({Key? key, required this.onLanguageChanged})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +63,7 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: Text(S.of(context).homeTitle),
       ),
       body: Center(
         child: Column(
@@ -49,7 +77,6 @@ class MyHomePage extends StatelessWidget {
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Navigate to Project List screen
                 Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -57,7 +84,7 @@ class MyHomePage extends StatelessWidget {
                   ),
                 );
               },
-              child: const Text('My Projects'),
+              child: Text(S.of(context).myProjects),
             ),
             const SizedBox(height: 10),
             ElevatedButton(
@@ -65,15 +92,51 @@ class MyHomePage extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => CreateProjectScreen(database: database),
+                    builder: (context) =>
+                        CreateProjectScreen(database: database),
                   ),
                 );
               },
-              child: const Text('Add New Project'),
+              child: Text(S.of(context).addNewProject),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _showLanguagePicker(context),
+              child: Text(S.of(context).changeLanguage),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showLanguagePicker(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(S.of(context).changeLanguage),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                title: const Text('English'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  onLanguageChanged(const Locale('en'));
+                },
+              ),
+              ListTile(
+                title: const Text('Magyar'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  onLanguageChanged(const Locale('hu'));
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
